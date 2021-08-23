c8d725750760884216c429d10c5edcb815ccd5b9
Yaro <yshmarov@gmail.com>
2021-08-23 21:30:03 +0000

    without enums

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,18 @@
 class Post < ApplicationRecord
   validates :title, presence: true
-  
+
+  STATUSES = [:draft, :published, :banned]
+
+  validates :status, inclusion: { in: Post::STATUSES }
+
+  scope :draft, -> { where(status: 'draft') }
+  scope :published, -> { where(status: 'published') }
+  scope :banned, -> { where(status: 'banned') }
+
+  def banned?
+    status == 'banned'
+  end
+
   def to_s
     title
   end
